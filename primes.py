bb4ae6512d02c5c14e059fde46452963dcad730e
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2023-11-24 13:30:19 -0600

    refactored primes file

--- primes.py
+++ primes.py
@@ -1,11 +1,8 @@
 primes = []
 upto = 100
 for n in range(2, upto + 1):
-    is_prime = True
     for divisor in range(2, n):
         if n % divisor == 0:
-            is_prime = False
             break
-    if is_prime:
-        primes.append(n)
+    else: primes.append(n)
 print(primes)
