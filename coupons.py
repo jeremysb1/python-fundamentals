60a02cb48d950da5288209c0dba7917bd3454c3c
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2023-11-24 13:49:30 -0600

    refactored program

--- coupons.py
+++ coupons.py
@@ -5,18 +5,17 @@ customers = [
     dict(id=4, total=100, coupon_code='F15'),
 ]
 
+discounts = {
+    'F20': (0.0, 20.0),
+    'P30': (0.3, 0.0),
+    'P50': (0.5, 0.0),
+    'F15': (0.0, 15.0),
+}
+
 for customer in customers:
     code = customer['coupon_code']
-    if code == 'F20':
-        customer['discount'] = 20.0
-    elif code == 'P30':
-        customer['discount'] = 15.0
-    elif code == 'P30':
-        customer['discount'] = customer['total'] * .3
-    elif code == 'P50':
-        customer['discount'] = customer['total'] * .5
-    else:
-        customer['discount'] = 0.0
+    percent, fixed = discounts.get(code, (0.0, 0.0))
+    customer['discount'] = percent * customer['total'] + fixed
 
 for customer in customers:
     print(customer['id'], customer['total'], customer['discount'])
