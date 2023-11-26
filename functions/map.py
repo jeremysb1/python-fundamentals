5552375d3cbdf3734cf905044cc2e74d264e820c
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2023-11-26 09:56:43 -0600

    added map function program

---
+++ functions/map.py
@@ -0,0 +1,6 @@
+print(map(lambda *a: a, range(3)))
+print(list(map(lambda *a: a, range(3))))
+print(list(map(lambda *a: a, range(3), 'abc')))
+print(list(map(lambda *a: a, range(3), 'abc', range(4, 7))))
+print(list(map(lambda *a: a, (), 'abc')))
+print(list(map(lambda *a: a, (1, 2, 3, 4), 'abc')))
