f3b7ade955e653d66eec55f2b6f96c88d40daf77
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2023-11-27 09:00:03 -0600

    combined two comprehensions into one

--- comprehensions-generators/pythagorean.triple.py
+++ comprehensions-generators/pythagorean.triple.py
@@ -1,5 +1,4 @@
 from math import sqrt
 mx = 10
-triples = [(a, b, sqrt(a**2 + b**2)) for a in range(1, mx) for b in range(a, mx)]
-triples = [(a, b, int(c)) for a, b, c in triples if c.is_integer()]
+triples = [(a, b, int(c)) for a in range(1, mx) for b in range(a, mx) if (c := sqrt(a**2 + b**2)).is_integer()]
 print(triples)
