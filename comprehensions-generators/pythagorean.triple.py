53fa4dc7d78cbf9cfb03960b89f7681d8bb8659b
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2023-11-26 21:58:07 -0600

    fixed program with map function

--- comprehensions-generators/pythagorean.triple.py
+++ comprehensions-generators/pythagorean.triple.py
@@ -1,5 +1,6 @@
 from math import sqrt
 mx = 10
 triples = [(a, b, sqrt(a**2 + b**2)) for a in range(1, mx) for b in range(a, mx)]
-triples = list(filter(lambda triple: triple[2].is_integer(), triples))
+triples = filter(lambda triple: triple[2].is_integer(), triples)
+triples = list(map(lambda triple: triple[:2] + (int(triple[2]), ), triples))
 print(triples)
