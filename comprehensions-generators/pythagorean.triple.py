bac770775ab39b5afb50aaafe8542b92b5c903d4
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2023-11-27 08:56:04 -0600

    refactored filter and map into list comprehension

--- comprehensions-generators/pythagorean.triple.py
+++ comprehensions-generators/pythagorean.triple.py
@@ -1,6 +1,5 @@
 from math import sqrt
 mx = 10
 triples = [(a, b, sqrt(a**2 + b**2)) for a in range(1, mx) for b in range(a, mx)]
-triples = filter(lambda triple: triple[2].is_integer(), triples)
-triples = list(map(lambda triple: triple[:2] + (int(triple[2]), ), triples))
+triples = [(a, b, int(c)) for a, b, c in triples if c.is_integer()]
 print(triples)
