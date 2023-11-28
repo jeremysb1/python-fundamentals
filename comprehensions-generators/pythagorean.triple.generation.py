110e5a4fa2bed5e401f0e971f5741ef71d11d1a4
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2023-11-28 14:20:40 -0600

    refactored program to improve readability

--- comprehensions-generators/pythagorean.triple.generation.py
+++ comprehensions-generators/pythagorean.triple.generation.py
@@ -1,10 +1,13 @@
 from functions import gcd
-N = 50
-triples = sorted(
-    ((a, b, c) for a, b, c in (
-        ((m**2 - n**2), (2 * m * n), (m**2 + n**2))
-        for m in range(1, int(N**.5) + 1)
-        for n in range(1, m)
-        if (m -n) % 2 and gcd(m, n) == 1
-    ) if c <= N), key=sum
-)
+
+def gen_triples(N):
+    for m in range(1, int(N**.5) + 1):
+        for n in range(1, m):
+            if (m-n) % 2 and gcd(m, n) == 1:
+                c = m**2 + n**2
+                if c <= N:
+                    a = m**2 - n**2
+                    b = 2 * m * n
+                    yield(a, b, c)
+
+sorted(gen_triples(50), key=sum)
