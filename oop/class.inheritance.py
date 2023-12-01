ca459de3c43e24998904a7fcc0f34dfe4ddc9978
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2023-12-01 17:24:18 -0600

    added class Car

--- oop/class.inheritance.py
+++ oop/class.inheritance.py
@@ -10,3 +10,19 @@ class ElectricEngine(Engine):
 class V8Engine(Engine):
     pass
 
+class Car:
+    engine_cls = Engine
+    def __init__(self):
+        self.engine = self.engine_cls()
+    def start(self):
+        print(
+            'Starting engine {0} for car {1}... Lets, go.'
+            .format(
+                self.engine.__class__.__name__,
+                self.__class__.__name__
+            )
+        )
+        self.engine.start()
+    def stop(self):
+        self.engine.stop()
+
