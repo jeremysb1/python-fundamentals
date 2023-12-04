466490f3f4e07dffff2fa8cbd9e40b33e2c2d77d
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2023-12-04 15:36:46 -0600

    added new instance

--- iterators/iterator.py
+++ iterators/iterator.py
@@ -1,10 +1,10 @@
 class OddEven:
     def __init__(self, data):
-        self.indexes = (list(range(0, len(data), 2)) + list(range(1, len(data), 2)))
-    
+        self._data = data
+        self.indexes = (list(range(0, len(data), 2)) +
+            list(range(1, len(data), 2)))
     def __iter__(self):
         return self
-    
     def __next__(self):
         if self.indexes:
             return self._data[self.indexes.pop(0)]
@@ -12,3 +12,6 @@ class OddEven:
 
 oddeven = OddEven('ThIsIsCoOl!')
 print(''.join(c for c in oddeven))
+oddeven = OddEven('CiAo')
+it = iter(oddeven)
+print(next(it))
