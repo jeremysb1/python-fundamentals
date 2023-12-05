3b990fe29237522b59edee682809dc71fa4fdb3f
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2023-12-04 18:56:48 -0600

    added module w/context manager

---
+++ context/manager.class.py
@@ -0,0 +1,12 @@
+class MyContextManager:
+    def __init__(self):
+        print("MyContextManager init", id(self))
+    
+    def __enter__(self):
+        print("Entering 'with' context")
+        return self
+    
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print(f"{exc_type=} {exc_val=} {exc_tb=}")
+        print("Exiting 'with' context")
+        return True
