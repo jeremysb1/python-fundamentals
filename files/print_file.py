e81576a1fef1753fdcbfe5b17df896ad5c163dbe
jeremysb1 <21281011+jeremysb1@users.noreply.github.com>
2023-12-06 10:24:14 -0600

    refactored program

--- files/print_file.py
+++ files/print_file.py
@@ -1,2 +1,4 @@
-with open('print_example.txt', 'w') as fw:
-    print('Printing to a file.', file=fw)
+with open('fear.txt') as f:
+    lines = [line.rstrip() for line in f]
+with open('fear_copy.txt', 'w') as fw:
+    fw.write('\n'.join(lines))
